ef8f12be922e4e6319dcd7779e72cd34c008eb9e
Sagar Gore <sagargore86@gmail.com>
2015-11-20 11:55:31 -0800

    Day5: Shared Preferences. Add refresh interval/broadcast receiver
    Added interval to refresh from yamba server and using broadcast notifier
    refresh database.

--- app/src/main/java/com/example_lab1/bootcamp/bootcamp_day1/SettingsFragment.java
+++ app/src/main/java/com/example_lab1/bootcamp/bootcamp_day1/SettingsFragment.java
@@ -1,13 +1,39 @@
 package com.example_lab1.bootcamp.bootcamp_day1;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
-public class SettingsFragment extends PreferenceFragment{
+public class SettingsFragment extends PreferenceFragment
+        implements SharedPreferences.OnSharedPreferenceChangeListener{
+
+    private SharedPreferences mPreferences;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        getActivity().sendBroadcast( new Intent(
+                "com.example_lab1.bootcamp.bootcamp_day1.action.UPDATED_INTERVAL") );
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mPreferences =
+                PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mPreferences.unregisterOnSharedPreferenceChangeListener(this);
     }
 }
